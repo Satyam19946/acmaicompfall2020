aa58eff1321e8fcf07806dfaae5383389c38506f
Satyam <sagupta@ucsd.edu>
2020-11-08 10:45:20 +0530

    All bots made

---
+++ allBots/energiuminerAct2.py
@@ -0,0 +1,139 @@
+from energium.game_constants import GAME_CONSTANTS, DIRECTIONS
+ALL_DIRECTIONS = [DIRECTIONS.EAST, DIRECTIONS.NORTH, DIRECTIONS.WEST, DIRECTIONS.SOUTH]
+from energium.kit import Agent
+import sys
+import math
+import random
+
+# Create new agent
+agent = Agent()
+
+# initialize agent
+agent.initialize()
+
+#print("My current energium is ", player.energium, file=sys.stderr)
+#print("Opponent energium is ", opponent.energium, file=sys.stderr)
+def perp_direction(direction):
+    """Returns a perpindicular direction to current"""
+    if direction == DIRECTIONS.NORTH:
+        return DIRECTIONS.EAST
+    elif direction == DIRECTIONS.EAST:
+        return DIRECTIONS.SOUTH
+    elif direction == DIRECTIONS.SOUTH:
+        return DIRECTIONS.WEST
+    elif direction == DIRECTIONS.WEST:
+        return DIRECTIONS.NORTH
+
+# Once initialized, we enter an infinite loop
+while True:
+
+    # wait for update from match engine
+    agent.update()
+
+    commands = []
+
+    ### AI Code goes here ###
+
+    player = agent.players[agent.id]
+    opponent = agent.players[(agent.id + 1) % 2]
+    unit_cost = GAME_CONSTANTS['PARAMETERS']['UNIT_COST'] #50
+    myUnits = player.units
+    myBases = player.bases
+    opponentBases = opponent.bases
+    opponentUnits = opponent.units
+    mapHeight = agent.mapHeight
+    mapWidth = agent.mapWidth
+    mymap = agent.map
+
+
+    # 1st try - Try implement a simple A* search algorithm 
+    energium_here = []
+    
+    opponentUnitsPos = []
+    for unit in opponentUnits:
+        opponentUnitsPos.append((unit.pos.x, unit.pos.y))
+
+    myUnitsPos = []
+    for unit in myUnits:
+        myUnitsPos.append((unit.pos.x, unit.pos.y))
+
+
+    if agent.turn % 5 == 0:
+        commands.append(myBases[random.randint(0,len(myBases)-1)].spawn_unit())
+        
+    for y in range(mapHeight):
+        for x in range(mapWidth):
+            if mymap.get_tile(x,y).energium:
+                energium_here.append([mymap.get_tile(x,y).energium,y,x,mymap.get_tile(x,y)])
+
+    energium_here.sort(reverse=True)
+
+    copyMyUnits = myUnits.copy()
+    for reachPoint in energium_here:
+        # For the point with the highest energium get the closest unit.
+        # Repeat for all
+        minCost, unitReq = [math.inf, None]
+        for unit in copyMyUnits:
+            dis = (reachPoint[1]-unit.pos.y)**2 + (reachPoint[2]-unit.pos.x)**2
+            if dis+unit.get_breakdown_level() < minCost:
+                minCost, unitReq = dis+unit.get_breakdown_level(), unit
+        if unitReq:
+            copyMyUnits.remove(unitReq)
+            direction_to_take = unitReq.pos.direction_to(reachPoint[3].pos)
+            if direction_to_take:
+                # pos_reached = unitReq.pos.translate(direction_to_take,1)
+                # while mymap.get_tile_by_pos(pos_reached).energium < 0:
+                #     direction_to_take = perp_direction(direction_to_take)
+                #     pos_reached = unitReq.pos.translate(direction_to_take,1)
+                commands.append(unitReq.move(direction_to_take))
+
+            # If moving is not possible, go to nearest base.
+            """
+            else:
+                minDis, baseReq = [math.inf, None]
+                for couldBeBase in myBases:
+                    if unitReq.pos.distance_to(couldBeBase.pos) < minDis:
+                        minDis = unitReq.pos.distance_to(couldBeBase.pos)
+                        baseReq = couldBeBase
+            
+                direction_to_take = unitReq.pos.direction_to(baseReq.pos)
+                if direction_to_take:
+                    commands.append(unitReq.move(direction_to_take))
+                else:
+                    commands.append(unitReq.move(ALL_DIRECTIONS[math.floor(random.random() * len(ALL_DIRECTIONS))]))
+            """ 
+    #After a unit reaches 20 breakdown level send it to nearest base.
+    for unit in myUnits:
+        if unit.get_breakdown_level() == 20:
+            collisionHappens = False
+            minDis, baseReq = [math.inf, None]
+            for couldBeBase in myBases:
+                if unit.pos.distance_to(couldBeBase.pos) < minDis:
+                    minDis = unit.pos.distance_to(couldBeBase.pos)
+                    baseReq = couldBeBase
+    
+            direction_to_take = unit.pos.direction_to(baseReq.pos)
+            if direction_to_take:
+                # Tile reached woule be
+                pos_reached = unit.pos.translate(direction_to_take,1)
+                for curUnit in myUnits:
+                    if curUnit.pos.equals(unit.pos):
+                        continue
+                    position_other_unit_reaches = curUnit.pos.translate(direction_to_take,1)
+                    if position_other_unit_reaches.equals(pos_reached):
+                        collisionHappens = True
+                        commands.append(unit.move(perp_direction(direction_to_take)))
+            if collisionHappens == False:
+                commands.append(unit.move(direction_to_take))
+
+
+    ### AI Code ends here ###
+
+
+    #print("The current commands are ", commands, file=sys.stderr)
+    # submit commands to the engine
+    print(','.join(commands))
+
+    # now we end our turn
+    agent.end_turn()
+
---
+++ allBots/simpleBot.py
@@ -0,0 +1,106 @@
+from energium.game_constants import GAME_CONSTANTS, DIRECTIONS
+ALL_DIRECTIONS = [DIRECTIONS.EAST, DIRECTIONS.NORTH, DIRECTIONS.WEST, DIRECTIONS.SOUTH]
+from energium.kit import Agent
+import sys
+import math
+import random
+from energium.position import Position
+# Create new agent
+agent = Agent()
+
+# initialize agent
+agent.initialize()
+
+# Once initialized, we enter an infinite loop
+while True:
+
+    # wait for update from match engine
+    agent.update()
+
+    commands = []
+
+    mymap = agent.map
+
+    # player is your player object, opponent is the opponent's
+    player = agent.players[agent.id]
+    opponent = agent.players[(agent.id + 1) % 2]
+
+    # use print("msg", file=sys.stderr) to print messages to the terminal or your error log.
+    # normal prints are reserved for the match engine. Uncomment the lines below to log something
+    # print('agent.turn, player.team, len(my_bases), len(my_units), player.energium)
+
+    random_base = random.randint(0,len(player.bases)-1)
+
+    totalCost = len(opponent.units)*50 + opponent.energium
+
+    if (len(opponent.units)*50 + opponent.energium) > (len(player.units)*50 + player.energium):
+        commands.append(player.bases[random_base].spawn_unit())
+
+    ### AI Code goes here ###
+    free_real_estate = []
+    tiles = []
+
+    for y in range(agent.mapHeight):
+        for x in range(agent.mapWidth):
+            if mymap.get_tile(x,y).energium >= 0:
+                flag = True
+                for unit in player.units:
+                    if unit.pos.equals(Position(x,y)):
+                        flag = False
+
+                for unit in opponent.units:
+                    if unit.pos.equals(Position(x,y)):
+                        flag = False
+
+                if flag:
+                    free_real_estate.append(mymap.get_tile(x,y).energium)
+                    tiles.append((x,y))
+
+
+    for unit in player.units:
+        max_energium = max(free_real_estate)
+        if mymap.get_tile_by_pos(unit.pos).energium >= max_energium:
+            continue
+        current_energium = mymap.get_tile_by_pos(unit.pos).energium
+        q = [(unit.pos.x,unit.pos.y)]
+        parents = {q[0]: q[0]}
+        visited = []
+        answer = None
+        while current_energium < max_energium and q:
+            # print(parents,file=sys.stderr)
+            current = q[0]
+            q = q[1:]
+            answer = current
+            visited.append(current)
+            current_energium = mymap.get_tile(current[0],current[1]).energium
+            positionsToAdd = [(current[0]+1,current[1]),(current[0],current[1]+1),(current[0]-1,current[1]),(current[0],current[1]-1)]
+            for add in positionsToAdd:
+                if add[0] >= 0 and add[0] < agent.mapWidth and add[1] >= 0 and add[1] < agent.mapHeight:
+                    if (add[0],add[1]) not in q and (add[0],add[1]) not in visited and (add[0],add[1]) in tiles:
+                            parents[(add[0],add[1])] = (current[0],current[1])
+                            q.append((add[0],add[1]))
+            
+        # print(parents[answer], current_energium,file=sys.stderr)
+        pos_to_go = (unit.pos.x,unit.pos.y)
+        while parents[answer] != (unit.pos.x,unit.pos.y):
+            answer = parents[answer]
+            # print(answer, "---", end="", file=sys.stderr)
+
+        # print("\n", answer, "===", pos_to_go, file=sys.stderr)
+        
+        direction = unit.pos.direction_to(Position(answer[0],answer[1]))
+        # print(pos_to_go,file=sys.stderr)
+        if direction:
+            free_real_estate.remove(max_energium)
+            tiles.remove(answer)
+            commands.append(unit.move(direction))
+
+    ### AI Code ends here ###
+
+
+    #print("The current commands are ", commands, file=sys.stderr)
+    # submit commands to the engine
+    print(','.join(commands))
+
+    # now we end our turn
+    agent.end_turn()
